a5f88bbe158ca9e9e1f07ffb63faa391b253fd24
GAHEE KIM <gaheekim610@gmail.com>
2023-09-01 13:32:08 +0900

    type guard and modules practice

---
+++ class-note/10_type-assertion.ts
@@ -0,0 +1,11 @@
+// 타입 단언 (type assertion)
+
+var a;
+a = 20;
+a = 'a';
+var b = a as string;
+
+// DOM API 조작
+// <div id="app">hi</div>
+var div = document.querySelector('div');
+var app = document.querySelector('#app');
---
+++ class-note/12_type-compatibility.ts
@@ -0,0 +1,15 @@
+// intereface
+
+interface Developer1 {
+  name: string;
+  skill: string;
+}
+interface Person1 {
+  name: string;
+}
+
+var developer : Developer1;
+var person: Person1;
+
+// developer = person;
+// person = developer;
